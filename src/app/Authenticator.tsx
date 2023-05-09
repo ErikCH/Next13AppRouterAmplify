690f9cf4e93eaf6f7ca6b8bf8bca40d6106a8647
Erik Hanchett <ehhanche@amazon.com>
2023-05-09 13:56:27 -0700

    Updated demo

--- src/app/Authenticator.tsx
+++ src/app/Authenticator.tsx
@@ -1,10 +1,15 @@
 "use client";
 import { Authenticator as AmplifyAuthenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
-import awsExports from "@/aws-exports";
+import awsExports from "../aws-exports";
 import "@aws-amplify/ui-react/styles.css";
 Amplify.configure(awsExports);
 
 export function Authenticator({ children }: { children?: React.ReactNode }) {
-  return <AmplifyAuthenticator>{children}</AmplifyAuthenticator>;
+  return (
+    <div>
+      <h1>test</h1>
+      <AmplifyAuthenticator>{children}</AmplifyAuthenticator>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
 import { Authenticator } from "./Authenticator";
+
+import { Amplify } from "aws-amplify";
+import awsExports from "@/aws-exports";
+Amplify.configure(awsExports);
 export default function Home() {
-  return <Authenticator></Authenticator>;
+  return (
+    <div>
+      <h1>Hi</h1>
+      Hello World
+      <Authenticator></Authenticator>
+    </div>
+  );
 }
